80340cb2d866325e14a8a546c8af76444d1c1e2d
Gregory <gregory.petrosyan@mail.utoronto.ca>
2017-02-27 20:38:14 -0500

    Add event bus

---
+++ src/bus.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+export default  new Vue({
+	data () {
+		return {
+			options: {}
+		};
+	}
+});
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import Editr from "./Editr.vue";
+import bus from "./bus.js";
 
 export default {
 	install: (Vue, options) => {
 		Vue.component("Editr", Editr);
+		bus.$set(bus.$data, "options", options);
+		bus.$emit("newOptions", options);
 	}
+
 }
