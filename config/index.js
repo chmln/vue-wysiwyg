3f7c64f32ddcdcbe4f16d962ce6c67faf68b52f1
Gregory <gregory.mkv@gmail.com>
2018-01-21 00:30:13 -0500

    config - rm obsolete static dir

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: './',
     assetsPublicPath: '/vue-wysiwyg/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
