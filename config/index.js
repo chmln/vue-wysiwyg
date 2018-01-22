f25fe6207d1e46025f24a5bf0981891ad7b19799
Gregory <gregory.mkv@gmail.com>
2018-01-22 16:29:30 -0500

    Update build config for demo site

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../' + process.env.DIR || "dist"),
     assetsSubDirectory: './',
     assetsPublicPath: '/vue-wysiwyg/',
     productionSourceMap: true,
