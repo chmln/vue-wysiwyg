6226de9eb1b7bcbd1725441950b8d5d47b99e9e9
Andy Epifani <andy.epifani@trademark.vision>
2018-06-14 09:48:53 +1000

    Added types; Code reformat; Paste in plain text

---
+++ lib/vue-wysiwyg-2.d.ts
@@ -0,0 +1,1 @@
+declare module 'vue-wysiwyg-2';
--- src/editor/Button.vue
+++ src/editor/Button.vue
@@ -15,57 +15,56 @@ div(@mousedown="onBtnClick")
       :uid="uid"
       :options="options"
     )
-
 </template>
 <script>
-import bus from 'src/editor/bus.js';
+    import bus from 'src/editor/bus.js';
 
-export default {
-	props: ["module", "options"],
+    export default {
+        props: ["module", "options"],
 
-	data () {
-		return {
-			showDashboard: false,
-		}
-	},
+        data() {
+            return {
+                showDashboard: false,
+            }
+        },
 
-  computed: {
-    uid () {
-      return this.$parent._uid
+        computed: {
+            uid() {
+                return this.$parent._uid
+            }
+        },
+
+        methods: {
+            closeDashboard() {
+                this.showDashboard = false;
+            },
+
+            openDashboard() {
+                this.showDashboard = true;
+            },
+
+            exec() {
+                this.$parent.exec.apply(null, arguments)
+            },
+
+            onBtnClick($event) {
+                $event.preventDefault();
+                if (this.module.action !== undefined)
+                    this.exec.apply(null, this.module.action);
+
+                else if (this.module.customAction !== undefined) {
+                    this.module.customAction(bus.utils).forEach(a => this.exec.apply(null, a));
+                }
+
+                else if (
+                    this.module.render !== undefined &&
+                    (!this.$refs.dashboard || !this.$refs.dashboard.contains($event.target))
+                ) {
+                    this.showDashboard = !this.showDashboard;
+                    bus.emit(`${this.uid}_${this.showDashboard ? "show" : "hide"}_dashboard_${this.module.title}`);
+                    return;
+                }
+            }
+        }
     }
-  },
-
-	methods: {
-		closeDashboard () {
-			this.showDashboard = false;
-		},
-
-		openDashboard () {
-			this.showDashboard = true;
-		},
-
-    exec () {
-      this.$parent.exec.apply(null, arguments)
-    },
-
-		onBtnClick ($event) {
-			$event.preventDefault();
-			if (this.module.action !== undefined)
-				this.exec.apply(null, this.module.action);
-
-			else if (this.module.customAction !== undefined) {
-				this.module.customAction(bus.utils).forEach(a => this.exec.apply(null, a));
-			}
-
-			else if (
-				this.module.render !== undefined &&
-				(!this.$refs.dashboard || !this.$refs.dashboard.contains($event.target))
-			) {
-				this.showDashboard = !this.showDashboard;
-				bus.emit(`${this.uid}_${this.showDashboard ? "show" : "hide"}_dashboard_${this.module.title}`);
-				return;
-			}
-		}
-	}
-}
 </script>
