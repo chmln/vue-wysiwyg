10a860b11116ae5b8bdd3a7b9c95b3c4d5f38de1
Járdi Mihály <jardi.m@outlook.com>
2020-03-27 17:49:30 +0100

    Added localization support (#113)
    * Localization support. Added default (english) and hungarian locales.
    
    * Removed 'this.'
    
    * Fixed locale specification
    
    * version bump, package name change
    
    * Added option to specify locale as a string
    
    * Update README.md (localization info)
    
    * Added localization example
    
    * Revert changes made to package.json
    
    * Prevent an error with custom modules on assigning titles
    
    Add one more member to the title value assignment, so if a custom module is not in the locale file, it won't show up as empty

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,8 +1,23 @@
 import Editr from "./Editr.vue";
 import bus from 'src/editor/bus.js';
+import locales from "./locales";
+
+const localeArray = Object.values(locales);
 
 export default {
 	install: (Vue, userOptions = {}) => {
+        if(userOptions.locale)
+        {
+            if(typeof userOptions.locale == 'string')
+            {
+                let userLocale = localeArray.find(
+                    x => x.locale_shorthand.includes(
+                        userOptions.locale.toLowerCase()
+                    )
+                );
+                userOptions.locale = userLocale;
+            }
+        }
 		bus.options = { ...bus.options, ...userOptions };
 		Vue.component("wysiwyg", Editr);
 	},
