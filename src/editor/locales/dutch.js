92e289cec2a8913d69a7f9eb937091e9541b6935
Daan Haitsma <daan@haitsma-dejager.nl>
2020-04-04 19:23:35 +0200

    Add Dutch translations (#114)

---
+++ src/editor/locales/dutch.js
@@ -0,0 +1,20 @@
+module.exports = {
+    locale_name: 'Dutch',
+    locale_shorthand: ['nl', 'dutch'],
+    // Translations:
+    justifyCenter: 'Gecentreerd uitlijnen',
+    justifyLeft: 'Links uitlijnen',
+    justifyRight: 'Rechts uitlijnen',
+    bold: 'Vet',
+    code: 'Code',
+    headings: 'Titels (h1-h6)',
+    link: 'Hyperlink',
+    image: 'Afbeelding invoegen',
+    italic: 'Cursief',
+    orderedList: 'Nummering (1, 2, 3, ...)',
+    unorderedList: 'Opsomming',
+    removeFormat: 'Opmaak verwijderen.\nMaakt titels, vetgedrukt, cursief, onderstreept etc. ongedaan.',
+    separator: null,
+    table: 'Tabel invoegen',
+    underline: 'Onderstrepen'
+}
