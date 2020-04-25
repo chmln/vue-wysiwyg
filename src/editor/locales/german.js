1cfeeb6a0ce57d695997fa7f8b089dce50c90578
flokX <flokX@users.noreply.github.com>
2020-04-25 16:36:43 +0200

    Create german.js

---
+++ src/editor/locales/german.js
@@ -0,0 +1,20 @@
+module.exports = {
+    locale_name: 'Deutsch',
+    locale_shorthand: ['de', 'deutsch'],
+    // Translations:
+    justifyCenter: 'Zentriert',
+    justifyLeft: 'Linksbündig',
+    justifyRight: 'Rechtsbündig',
+    bold: 'Fett',
+    code: 'Programmcode',
+    headings: 'Überschriften (h1-h6)',
+    link: 'Link',
+    image: 'Bild einfügen',
+    italic: 'Kursiv',
+    orderedList: 'Nummerierung (1, 2, 3, ...)',
+    unorderedList: 'Aufzählung',
+    removeFormat: 'Formatierung entfernen.\nEntfernt Überschriften, fett, kursiv, unterstrichen, usw.',
+    separator: 'Querstrich',
+    table: 'Tabelle einfügen',
+    underline: 'Unterstrichen'
+}
