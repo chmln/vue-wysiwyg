6f8c3751b61682ff51166e7f576572a1b23d956e
Adryan Almeida <adryan1990@gmail.com>
2020-05-06 01:00:19 -0300

    Add Portuguese translations

--- src/editor/locales/index.js
+++ src/editor/locales/index.js
@@ -1,9 +1,11 @@
-import dutch from './dutch';
-import hungarian from './hungarian';
-import english from './english';
+import dutch from "./dutch";
+import hungarian from "./hungarian";
+import english from "./english";
+import portuguese from "./portuguese";
 
 export default {
-    dutch,
-    hungarian,
-    english
-}
+  dutch,
+  hungarian,
+  english,
+  portuguese,
+};
---
+++ src/editor/locales/portuguese.js
@@ -0,0 +1,21 @@
+module.exports = {
+  locale_name: "Português",
+  locale_shorthand: ["pt", "portuguese"],
+  // Traduções:
+  justifyCenter: "Centralizar",
+  justifyLeft: "Justificar à Esquerda",
+  justifyRight: "Justificar à Direita",
+  bold: "Negrito",
+  code: "Código",
+  headings: "Cabeçalhos (h1-h6)",
+  link: "Hiperlink",
+  image: "Inserir Imagem",
+  italic: "Itálico",
+  orderedList: "Lista Ordenada (1, 2, 3, ...)",
+  unorderedList: "Lista Desordenada",
+  removeFormat:
+    "Remove formatação.\nLimpa cabeçalhos, negrito, itálico, texto sublinhado, etc.",
+  separator: null,
+  table: "Inserir Tabela",
+  underline: "Sublinhado",
+};
