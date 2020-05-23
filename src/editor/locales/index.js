1a8836474ae7dffd10dc228e4ec3189f2b8227ef
flokX <flokX@users.noreply.github.com>
2020-05-23 07:40:35 +0200

    Add german translation (#117)
    * Create german.js
    
    * Add german translation

--- src/editor/locales/index.js
+++ src/editor/locales/index.js
@@ -1,9 +1,11 @@
 import dutch from './dutch';
 import hungarian from './hungarian';
+import german from './german';
 import english from './english';
 
 export default {
     dutch,
     hungarian,
+    german,
     english
 }
