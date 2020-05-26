2c9c0ae1aecf92c3b5dba6ff11b581ba26101364
Cesar Ureño <cesarurenomartinez@gmail.com>
2020-05-25 19:00:32 -0500

    Create spanish.js

---
+++ src/editor/locales/spanish.js
@@ -0,0 +1,20 @@
+module.exports = {
+    locale_name: 'Español',
+    locale_shorthand: ['es', 'español'],
+    // Translations:
+    justifyCenter: 'Centrar',
+    justifyLeft: 'Justificar a la Izquierda',
+    justifyRight: 'Justificar a la Derecha',
+    bold: 'Negrita',
+    code: 'Código',
+    headings: 'Encabezados (h1-h6)',
+    link: 'Hipervínculo',
+    image: 'Insertar Imagen',
+    italic: 'Itálica',
+    orderedList: 'Lista Ordenanda (1, 2, 3, ...)',
+    unorderedList: 'Lista Desordenada',
+    removeFormat: 'Eliminar Formato.\nBorra encabezados, negritas, cursiva, text subrayado, etc.',
+    separator: null,
+    table: 'Insertar Tabla',
+    underline: 'Subrayado'
+}
