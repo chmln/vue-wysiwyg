c7e561977c6f7130abbef84064d1060b4edbb2ab
technolog <technologies.bro@gmail.com>
2020-08-19 19:52:43 +0300

    add:russian translate

---
+++ src/editor/locales/russian.js
@@ -0,0 +1,20 @@
+module.exports = {
+  locale_name: 'Russian',
+  locale_shorthand: ['ru', 'russian'],
+  // Translations:
+  justifyCenter: 'По центру',
+  justifyLeft: 'Слева',
+  justifyRight: 'Справа',
+  bold: 'Жирный',
+  code: 'Код',
+  headings: 'Заголовки (h1-h6)',
+  link: 'Гиперссылка',
+  image: 'Вставить изображение',
+  italic: 'Наклонный',
+  orderedList: 'Список с нумерацией (1, 2, 3, ...)',
+  unorderedList: 'Маркерный список',
+  removeFormat: 'Удалить форматирование.\nОчистить заголовки, жирный, наклонный, подчёркнутый текст, итд.',
+  separator: null,
+  table: 'Вставить Таблицу',
+  underline: 'Подчёркнутый'
+}
