9dfbcdc1ea5c3bc9eefabbd8ad1da99fd0872355
Phu.Nguyen <Phu.Nguyen@altsrc.net>
2020-11-25 10:18:31 +0700

    fix to german

--- src/editor/locales/index.js
+++ src/editor/locales/index.js
@@ -2,13 +2,13 @@ import dutch from "./dutch";
 import hungarian from "./hungarian";
 import english from "./english";
 import portuguese from "./portuguese";
-import italian from "./italian";
+import german from './german';
 import spanish from "./spanish";
 
 export default {
     dutch,
     hungarian,
-    italian,
+    german,
     english,
     portuguese,
     spanish,
